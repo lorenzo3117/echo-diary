49666e364ab65464196807d8fa7e81fdeb5915ba
Lorenzo Catalano <lorenzo.catalano@dxc.com>
2025-08-03 00:04:29 +0200

    Add Trix attachment upload and deletion support with improved editor integration
    - Implement `TrixAttachmentService` for handling file uploads and deletions.
    - Add `TrixAttachmentController` and define a new route for Trix file uploads.
    - Update `PostController` to remove post attachments on deletion.
    - Enhance `trix-input` with AlpineJS for real-time progress tracking during file uploads.
    - Introduce new `trix.css` styles and integrate into the build process.

--- resources/views/components/stats/list.blade.php
+++ resources/views/components/stats/list.blade.php
@@ -3,7 +3,7 @@
 ])
 
 @if($stats)
-    <div class="stats stats-vertical lg:stats-horizontal shadow w-full">
+    <div class="stats stats-vertical md:stats-horizontal shadow w-full">
         @foreach($stats as $stat)
             <x-stats.item :title="$stat['title'] ?? ''" :value="$stat['value'] ?? ''" :description="$stat['description'] ?? ''"/>
         @endforeach
