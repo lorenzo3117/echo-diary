b9a71c513030a46aef7af0f360269380e68784c3
Lorenzo Catalano <lorenzo.catalano@dxc.com>
2025-08-09 16:56:49 +0200

    Update middleware to trust all proxies
    - Add `trustProxies` call to middleware configuration with wildcard.

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,6 +13,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->appendToGroup('web', LocaleMiddleware::class);
+        $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
